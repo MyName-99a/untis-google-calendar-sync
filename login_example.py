2b01eca33d9e0a74e8a7945fdf069b5465f1cc7e
MyName99a <77343596+MyName-99a@users.noreply.github.com>
2026-02-03 09:30:36 +0100

    Added login_example and requirements

---
+++ login_example.py
@@ -0,0 +1,7 @@
+# Daten für Webuntis und die schule
+user = '' # Den username
+password = '' # das passwort dazu
+server = '' # url der schule e.g.: 'schule.webuntis.com'
+
+# Für die NTFY App
+ntfy_topic = "" # ein name für die ntfy app um benachrichtigungen auf das handy zu bekommen, falls sich etwas ändert
